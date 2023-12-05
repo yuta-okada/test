f61215688c4bc833886a225e828a902157faf7f8
yuta-okada <yutafukudai0822@gmail.com>
2023-12-05 10:49:45 +0900

    3番目のコミットメッセージです。

--- login-input.php
+++ login-input.php
@@ -3,6 +3,54 @@
 <head>
     <title>ユーザー登録</title>
 </head>
+<style>
+        h1 {
+            color: #333;
+            text-align: center;
+            margin-top: 50px;
+        }
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+            color: #333;
+        }
+
+        input {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 16px;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            background-color: #4caf50;
+            color: #fff;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+</style>
 <body>
     <h1>ユーザー登録</h1>
     <form action="login_pdo.php" method="post">
--- product.php
+++ product.php
@@ -1,4 +1,34 @@
 <?php require 'menu.php'; ?>
+<style>
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-top: 10px;
+    }
+
+    th, td {
+        border: 1px solid #ddd;
+        padding: 8px;
+        text-align: left;
+    }
+
+    th {
+        background-color: #f2f2f2;
+    }
+
+    td a {
+        text-decoration: none;
+        color: #333;
+        font-weight: bold;
+        display: block; /* Make the entire cell clickable */
+        padding: 8px; /* Add padding for better clickability */
+    }
+
+    td a:hover {
+        color: #0066cc;
+        background-color: #f2f2f2; /* Highlight on hover */
+    }
+</style>
 <form action="product.php" method="post">
 商品検索
 <input type="text" name="keyword">
@@ -7,23 +37,36 @@
 <hr>
 <?php
 echo '<table>';
-echo '<tr><th>商品番号</th><th>商品名</th><th>価格</th><th>カテゴリー</th><th>サイズ</th></th>カラー</th><th>在庫</th></tr>';
-$pdo=new PDO('mysql:host=localhost;dbname=clothes;charset=utf8', 
-    'root', '');
+echo '<tr><th>カテゴリー</th><th>商品名</th></tr>';
+$pdo = new PDO('mysql:host=localhost;dbname=clothes;charset=utf8', 'root', '');
+
 if (isset($_REQUEST['keyword'])) {
-    $sql=$pdo->prepare('select * from product where name like ?');
-    $sql->execute(['%'.$_REQUEST['keyword'].'%']);
+    $sql = $pdo->prepare('SELECT category, name, id FROM product WHERE name LIKE ?');
+    $sql->execute(['%' . $_REQUEST['keyword'] . '%']);
 } else {
-    $sql=$pdo->query('select * from product');
+    $sql = $pdo->query('SELECT category, name, id FROM product');
 }
+
+// カテゴリー別に商品名をグループ化
+$groupedProducts = [];
 foreach ($sql as $row) {
-    $id=$row['id'];
+    $category = $row['category'];
+    $productName = $row['name'];
+    $productId = $row['id'];
+    if (!isset($groupedProducts[$category][$productName])) {
+        $groupedProducts[$category][$productName] = $productId;
+    }
+}
+
+// グループ化された商品を表示
+foreach ($groupedProducts as $category => $productNames) {
     echo '<tr>';
-    echo '<td>', $id, '</td>';
+    echo '<td style="font-weight: bold;">', $category, '</td>';
     echo '<td>';
-    echo '<a href="detail.php?id=', $id, '">', $row['name'], '</a>';
+    foreach ($productNames as $productName => $productId) {
+        echo '<a href="detail.php?id=', $productId, '">', $productName, '</a>';
+    }
     echo '</td>';
-    echo '<td>', $row['price'], '</td>';
     echo '</tr>';
 }
 echo '</table>';
